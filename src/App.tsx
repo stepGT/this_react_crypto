dd4a335d22901b7e870e1346dac8f32cb23ab3c8
Andrey Zuev <stepgt@mail.ru>
2024-03-18 10:44:13 +1000

    Added Layout, Header, Sider and Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import React from 'react';
 import './App.css';
+import { Layout } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 60,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 60px)',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
 
 const App = () => {
-  return <>React Crypto</>;
+  return (
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider width="25%" style={siderStyle}>
+          Sider
+        </Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
