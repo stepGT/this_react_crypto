5d5beb7f0c1f1b462f6f7d3db24d907de60562a0
Andrey Zuev <stepgt@mail.ru>
2024-03-29 13:42:10 +1000

    Added Select to AddAssetForm

--- src/components/AddAssetForm.tsx
+++ src/components/AddAssetForm.tsx
@@ -1,5 +1,35 @@
-﻿const AddAssetForm = () => {
-  return <form>FORM</form>
+﻿import { useState } from 'react';
+import { Select, Space } from 'antd';
+import { TCryptoData } from '../data';
+import { useCrypto } from '../context/crypto-context';
+
+const AddAssetForm = () => {
+  const [coin, setCoin] = useState<TCryptoData>();
+  const { crypto } = useCrypto();
+  //
+  if (!coin) {
+    return (
+      <Select
+        style={{
+          width: '100%',
+        }}
+        onSelect={(v) => setCoin(crypto?.result.find((c) => c.id === v))}
+        placeholder="Select coin"
+        options={crypto.result.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img style={{ width: 20 }} src={option.data.icon} alt={option.data.label} />
+            {option.data.label}
+          </Space>
+        )}
+      />
+    );
+  }
+  return <form>FORM</form>;
 };
 
 export default AddAssetForm;
