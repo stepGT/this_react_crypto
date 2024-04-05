083e83e7914837d4b574edc24898ebac8f8c58bd
Andrey Zuev <stepgt@mail.ru>
2024-04-05 11:31:58 +1000

    Refactor setAssets

--- src/context/crypto-context.tsx
+++ src/context/crypto-context.tsx
@@ -52,18 +52,7 @@ export function CryptoContextProvider({ children }: PropsWithChildren) {
       const dataAssets = await fetchAssets();
       //
       setCrypto(dataCrypto);
-      setAssets(
-        dataAssets.map((asset) => {
-          const coin = dataCrypto.result.find((crypto) => crypto.id === asset.id)!;
-          return {
-            grow: asset.price < coin.price,
-            growPercent: percentDifference(asset.price, coin.price),
-            totalAmount: asset.amount * coin.price,
-            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-            ...asset,
-          };
-        }),
-      );
+      setAssets(mapAssets(dataAssets, dataCrypto));
       setLoading(false);
     };
     preload();
